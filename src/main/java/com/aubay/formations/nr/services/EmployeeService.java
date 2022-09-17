f6b6bfca12feb3ad2bbf07ff66f0725ce1a9eb5a
Julien Bureau <ankori92@gmail.com>
2022-09-17 22:27:52 +0200

    Problème N+1 requêtes

--- src/main/java/com/aubay/formations/nr/services/EmployeeService.java
+++ src/main/java/com/aubay/formations/nr/services/EmployeeService.java
@@ -1,11 +1,18 @@
 package com.aubay.formations.nr.services;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +20,7 @@ import com.aubay.formations.nr.entities.Country;
 import com.aubay.formations.nr.entities.Employee;
 import com.aubay.formations.nr.repositories.CountryRepository;
 import com.aubay.formations.nr.repositories.EmployeeRepository;
+import com.aubay.formations.nr.utils.Chrono;
 
 /**
  * Employees and Teams services
@@ -28,23 +36,6 @@ public class EmployeeService {
 
 	@Autowired
 	private CountryRepository countryRepository;
-
-	/**
-	 * Find an Employee by his ID
-	 *
-	 * @param id
-	 * @param filterResigned true if resigned employees must be removed from result,
-	 *                       false else
-	 * @return
-	 */
-	public Employee findEmployee(final long id, final boolean filterResigned) {
-		final var employee = employeeRepository.getReferenceById(id);
-		if (filterResigned) {
-			filterResignedEmployees(employee);
-		}
-		hibernateRecursiveInitialization(employee);
-		return employee;
-	}
 
 	/**
 	 * Create or update employee
@@ -68,12 +59,102 @@ public class EmployeeService {
 	public List<Employee> getTopManagers(final boolean filterResigned) {
 		final var employees = employeeRepository.findByManagerNullAndResignedFalse();
 		for (final Employee e : employees) {
-			hibernateRecursiveInitialization(e);
-			if (filterResigned) {
-				filterResignedEmployees(e);
-			}
+			initializeEmployee(e);
 		}
 		return employees;
+	}
+
+	/**
+	 * Build the manager's team with only 2 queries (Employees, and countries)<br />
+	 * May be optimized with JOIN, but first query is already complex.<br />
+	 * A team is the manager (Employee object) and employees under the
+	 * responsibility of the manager (List<Employee> employees)<br />
+	 * Warning : A sub-employee may be also manager of his proper team
+	 *
+	 * @param manager not initialized
+	 */
+	public void initializeEmployee(final Employee manager) {
+		Chrono.start();
+		// Get all concerned Employees (ONE SHOT QUERY)
+		final var employeesInManagerTeam = employeeRepository.findEmployeesInTree(manager.getId());
+		Chrono.trace("Employees loaded", employeesInManagerTeam.size());
+
+		// Get all concerned Countries (ONE SHOT QUERY)
+		final var countriesId = employeesInManagerTeam.stream().map(e -> (String) e.get("country_id")).toList();
+		final var countries = countryRepository.findAllById(countriesId).stream()
+				.collect(Collectors.toMap(Country::getCode, Function.identity()));
+		Chrono.trace("Countries loaded", countries.size());
+
+		// NO QUERIES AFTER THIS POINT
+		// Rebuild all teams
+		final Map<Long, Set<Long>> map = new HashMap<>();
+		for (final Map<String, Object> emp : employeesInManagerTeam) {
+			if (emp.get("manager_id") == null) {
+				continue;
+			}
+			final var managerId = ((BigInteger) emp.get("manager_id")).longValue();
+			var team = map.get(managerId);
+			if (team == null) {
+				team = new HashSet<>();
+			}
+			team.add(((BigInteger) emp.get("id")).longValue());
+			map.put(managerId, team);
+		}
+		Chrono.trace("Analyzing employees relations");
+
+		// Organize all teams and set final team
+		manager.setEmployees(rebuildTeam(map.get(manager.getId()), employeesInManagerTeam, map, countries));
+		Chrono.trace("Rebuilt employees relations");
+		Chrono.stop();
+	}
+
+	/**
+	 * Rebuilt manager's team, recursively
+	 * @formatter:off
+	 * @param employeesInThisTeam
+	 * @param allEmployees
+	 * @param teamByManager
+	 * @param allCountries
+	 * @return
+	 */
+	private List<Employee> rebuildTeam(final Set<Long> employeesInThisTeam, final List<Map<String, Object>> allEmployees,
+			final Map<Long, Set<Long>> teamByManager, final Map<String, Country> allCountries) {
+		final var result = new ArrayList<Employee>();
+		if (employeesInThisTeam == null) {
+			return result;
+		}
+		for (final Long employeeId : employeesInThisTeam) {
+			final var empData = getEmployeeInList(allEmployees, employeeId);
+			if((boolean) empData.get("resigned")) {
+				continue;
+			}
+			final var team = rebuildTeam(teamByManager.get(((BigInteger) empData.get("id")).longValue()), allEmployees, teamByManager, allCountries);
+			final var emp = Employee.builder()
+					.id(((BigInteger) empData.get("id")).longValue())
+					.firstname((String) empData.get("firstname"))
+					.lastname((String) empData.get("lastname"))
+					.entryDate((Date) empData.get("entry_date"))
+					.resigned((boolean) empData.get("resigned"))
+					.salary((int) empData.get("salary"))
+					.country(allCountries.get(empData.get("country_id")))
+					.employees(team)
+					.build();
+			emp.setEmployees(team);
+			result.add(emp);
+		}
+		return result;
+	}
+
+	/**
+	 * @formatter:on
+	 */
+	public Map<String, Object> getEmployeeInList(final List<Map<String, Object>> allEmps, final long id) {
+		for (final Map<String, Object> emp : allEmps) {
+			if (id == ((BigInteger) emp.get("id")).longValue()) {
+				return emp;
+			}
+		}
+		return null;
 	}
 
 	/**
@@ -99,17 +180,5 @@ public class EmployeeService {
 			}
 		}
 		employee.setEmployees(result);
-	}
-
-	/**
-	 * Initialize employees (Hibernate LAZY entities links)
-	 *
-	 * @param employees
-	 */
-	public void hibernateRecursiveInitialization(final Employee employee) {
-		for (final Employee e : employee.getEmployees()) {
-			hibernateRecursiveInitialization(e);
-			Hibernate.initialize(e);
-		}
 	}
 }

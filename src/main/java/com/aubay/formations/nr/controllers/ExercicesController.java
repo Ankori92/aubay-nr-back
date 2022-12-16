97dc05d02a56349c859b05af75cb5d2e0d2cd6bc
Julien Bureau <ankori92@gmail.com>
2022-12-16 17:20:59 +0100

    Correction merge

--- src/main/java/com/aubay/formations/nr/controllers/ExercicesController.java
+++ src/main/java/com/aubay/formations/nr/controllers/ExercicesController.java
@@ -3,6 +3,7 @@ package com.aubay.formations.nr.controllers;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 import javax.transaction.Transactional;
 
